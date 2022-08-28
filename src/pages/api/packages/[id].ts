4f051bf0d4f39254a50fcebfb5ddd72de977a8c9
Tropical <42101043+Tropix126@users.noreply.github.com>
2022-08-28 14:46:05 -0500

    downgrade solid (1.5 is buggy)

--- src/pages/api/packages/[id].ts
+++ src/pages/api/packages/[id].ts
@@ -1,18 +1,18 @@
 import type { Package } from "~/lib/packages";
 
 export async function get({ params }) {
-    const modules = await import.meta.glob("~/packages/*.json");
-    const packages: Package[] = [];
+  const modules = await import.meta.glob("~/packages/*.json");
+  const packages: Package[] = [];
 
-    for (const module of Object.values(modules)) {
-        packages.push((await module()).default);
-    }
+  for (const module of Object.values(modules)) {
+    packages.push((await module()).default);
+  }
 
-    const match = packages.find(pkg => pkg.id === params.id);
+  const match = packages.find(pkg => pkg.id === params.id);
 
-    if (!match) return new Response(`Could not find "${params.id}"`, { status: 404 });
+  if (!match) return new Response(`Could not find "${params.id}"`, { status: 404 });
 
-    return new Response(JSON.stringify(match), {
-      status: 200
-    });
+  return new Response(JSON.stringify(match), {
+    status: 200
+  });
 }
